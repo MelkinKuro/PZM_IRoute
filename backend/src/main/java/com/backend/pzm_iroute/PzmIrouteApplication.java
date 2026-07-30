6cd7da8676924ac9e9a5095aa69551d438a39b3a
MelkinKuro <elkin73mendoza@gmail.com>
2026-07-30 14:08:11 -0500

    Estructura frond y back

---
+++ backend/src/main/java/com/backend/pzm_iroute/PzmIrouteApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pzm_iroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PzmIrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PzmIrouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/pzm_iroute/PzmIrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pzm_iroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PzmIrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
